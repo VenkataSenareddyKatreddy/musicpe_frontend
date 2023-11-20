a29f2999c5f680e5c853b726797a2ce089ba3245
VenkataSenareddyKatreddy <144657344+VenkataSenareddyKatreddy@users.noreply.github.com>
2023-11-20 20:30:38 +0530

    Update UploadSong.jsx

--- src/pages/UploadSong.jsx
+++ src/pages/UploadSong.jsx
@@ -132,7 +132,7 @@ const UploadSong = () => {
           type="submit"
           disabled={localStorage.getItem("access_token") ? false : true}
         >
-          Submit
+          Upload
         </button>
       </form>
     </div>
